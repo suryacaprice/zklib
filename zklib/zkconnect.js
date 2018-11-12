8f9f057d2e683c9d4cdff3f83cffc1f01f85ec0f
Surya Kumaran <surya.kumaran@matrixthread.com.au>
2018-11-12 16:02:02 +0530

    First push

---
+++ zklib/zkconnect.js
@@ -0,0 +1,43 @@
+const dgram = require('dgram');
+
+const {Commands} = require('./constants');
+
+module.exports = class {
+  /**
+   *
+   * @param {(error?: Error) => void} [cb]
+   */
+  connect(cb) {
+    this.createSocket(err => {
+      if (err) {
+        cb(err);
+
+        return;
+      }
+
+      this.executeCmd(Commands.CONNECT, '', err => {
+        if (err) {
+          this.closeSocket();
+
+          cb(err);
+
+          return;
+        }
+
+        cb();
+      });
+    });
+  }
+
+  /**
+   *
+   * @param {(error: Error) => void} [cb]
+   */
+  disconnect(cb) {
+    this.executeCmd(Commands.EXIT, '', err => {
+      this.closeSocket();
+
+      // cb(err);
+    });
+  }
+};
